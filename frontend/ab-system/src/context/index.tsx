3124b7e39b97cbdb49f0e13b90b25ca37f7f0a4e
SynnekOG <ShaneRugget@outlook.com>
2025-10-11 20:30:36 -0700

    feat: Update /context/index.tsx file & npm install

--- frontend/ab-system/src/context/index.tsx
+++ frontend/ab-system/src/context/index.tsx
@@ -11,9 +11,9 @@ const queryClient = new QueryClient()
 
 // Set up metadata
 const metadata = {
-  name: 'next-reown-appkit',
-  description: 'next-reown-appkit',
-  url: 'https://github.com/0xonerb/next-reown-appkit-ssr', // origin must match your domain & subdomain
+  name: 'ab-system',
+  description: 'A/B Testing System',
+  url: 'https://ab-system.vercel.app', // origin must match your domain & subdomain
   icons: ['https://avatars.githubusercontent.com/u/179229932']
 }
 
