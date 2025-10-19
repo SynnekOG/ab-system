33dd0dfcc2f577ec3b7050e481bc5df180115423
SynnekOG <ShaneRugget@outlook.com>
2025-10-18 19:43:06 -0700

    feat: forge fmt

--- src/ABContract.sol
+++ src/ABContract.sol
@@ -6,8 +6,9 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract ABContract is ERC721, ERC721URIStorage, Ownable {
+    uint256 private _tokenIds;
 
-        constructor(string memory name, string memory symbol, address initialOwner)
+    constructor(string memory name, string memory symbol, address initialOwner)
         ERC721(name, symbol)
         Ownable(initialOwner)
     {
