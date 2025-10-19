18dbef36c9e6e1e73afc2df91d509dd21dd7fee5
SynnekOG <ShaneRugget@outlook.com>
2025-10-18 19:41:36 -0700

    feat: Add Constructor Function

--- src/ABContract.sol
+++ src/ABContract.sol
@@ -5,4 +5,13 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract ABContract is ERC721, ERC721URIStorage, Ownable {}
+contract ABContract is ERC721, ERC721URIStorage, Ownable {
+
+        constructor(string memory name, string memory symbol, address initialOwner)
+        ERC721(name, symbol)
+        Ownable(initialOwner)
+    {
+        // Start token IDs at 1
+        _tokenIds = 1;
+    }
+}
