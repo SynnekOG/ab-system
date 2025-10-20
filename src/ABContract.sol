5eed83e060b65caf61f542747454e0607cb7ae4e
SynnekOG <ShaneRugget@outlook.com>
2025-10-20 11:09:20 -0700

    feat: Add BadgeMetadata Struct

--- src/ABContract.sol
+++ src/ABContract.sol
@@ -8,6 +8,16 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract ABContract is ERC721, ERC721URIStorage, Ownable {
     uint256 private _tokenIds;
 
+    // Badge metadata structure
+    struct BadgeMetadata {
+        string name;
+        string description;
+        uint256 achievementId;
+        uint256 earnedTimestamp;
+        uint8 rarity; // 1=Common, 2=Rare, 3=Epic, 4=Legendary
+        bool soulbound;
+    }
+    
     constructor(string memory name, string memory symbol, address initialOwner)
         ERC721(name, symbol)
         Ownable(initialOwner)
