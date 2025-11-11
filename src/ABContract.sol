966cc3a8cc1f7c56e0722fc4578d7ceddb9ad2dc
SynnekOG <ShaneRugget@outlook.com>
2025-11-10 17:16:03 -0800

    feat: forge fmt

--- src/ABContract.sol
+++ src/ABContract.sol
@@ -174,7 +174,7 @@ contract ABContract is ERC721, ERC721URIStorage, Ownable {
         userBadges[to].push(tokenId);
     }
 
-        function _removeFromUserBadges(address user, uint256 tokenId) private {
+    function _removeFromUserBadges(address user, uint256 tokenId) private {
         uint256[] storage badges = userBadges[user];
         for (uint256 i = 0; i < badges.length; i++) {
             if (badges[i] == tokenId) {
@@ -183,7 +183,8 @@ contract ABContract is ERC721, ERC721URIStorage, Ownable {
                 break;
             }
         }
-        
+    }
+
     // Override required by Solidity for multiple inheritance
     function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
         return super.tokenURI(tokenId);
