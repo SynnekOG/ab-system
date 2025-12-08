4bcb4d7d2a0ded5d16c0c30a42dfa9be70b49735
SynnekOG <ShaneRugget@outlook.com>
2025-12-07 21:03:30 -0800

    feat: Add updateAchievementThresholds External Admin Function

--- src/ABManager.sol
+++ src/ABManager.sol
@@ -262,4 +262,9 @@ contract ABManager is Ownable, ReentrancyGuard {
         achievements[achievementId].isActive = isActive;
         emit AchievementUpdated(achievementId);
     }
+
+        function updateAchievementThresholds(uint256 achievementId, uint256[] memory newThresholds) external onlyOwner {
+        achievements[achievementId].thresholds = newThresholds;
+        emit AchievementUpdated(achievementId);
+    }
 }
