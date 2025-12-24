ac494cc7d32664fe82b61a0207e6c86b8cf8f38d
SynnekOG <ShaneRugget@outlook.com>
2025-12-24 14:38:35 -0800

    feat: Add Contract Title Natspec Comments

--- src/ABTracker.sol
+++ src/ABTracker.sol
@@ -5,4 +5,8 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "./interface/IActivityTracker.sol";
 
+/**
+ * @title ABTracker
+ * @dev Tracks DeFi-related activities like swaps, liquidity provision, lending, etc.
+ */
 contract ABTracker {}
