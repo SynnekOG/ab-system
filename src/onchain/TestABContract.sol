8c94d9c56d4213b440cfc820ac5fd812e714a3a7
SynnekOG <ShaneRugget@outlook.com>
2025-12-24 14:36:12 -0800

    feat: Mainnet Test Deployments

--- src/onchain/TestABContract.sol
+++ src/onchain/TestABContract.sol
@@ -7,7 +7,7 @@ contract TestABContract {
     string public name;
     string public symbol;
     uint256 private _tokenIds;
-    string private version = "1.0.29";
+    string private version = "1.0.33";
 
     struct Badge {
         string badgeName;
