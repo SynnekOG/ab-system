282ddeaca2aa817d554b45e2a211777fdea939d5
SynnekOG <ShaneRugget@outlook.com>
2025-12-29 19:43:45 -0800

    feat: forge fmt

--- src/ABTracker.sol
+++ src/ABTracker.sol
@@ -15,9 +15,8 @@ contract ABTracker {
     bytes32 public constant LIQUIDITY_ADD = keccak256("LIQUIDITY_ADD");
     bytes32 public constant LIQUIDITY_REMOVE = keccak256("LIQUIDITY_REMOVE");
     bytes32 public constant LENDING = keccak256("LENDING");
-        bytes32 public constant BORROWING = keccak256("BORROWING");
+    bytes32 public constant BORROWING = keccak256("BORROWING");
     bytes32 public constant STAKING = keccak256("STAKING");
     bytes32 public constant YIELD_FARMING = keccak256("YIELD_FARMING");
     bytes32 public constant NFT_TRADE = keccak256("NFT_TRADE");
-
 }
