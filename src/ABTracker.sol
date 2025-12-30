5e6f8590ec8cb38689b0da8cbee2a7987e68e544
SynnekOG <ShaneRugget@outlook.com>
2025-12-29 19:40:58 -0800

    feat: Add bytes32 public constant STAKING

--- src/ABTracker.sol
+++ src/ABTracker.sol
@@ -16,5 +16,6 @@ contract ABTracker {
     bytes32 public constant LIQUIDITY_REMOVE = keccak256("LIQUIDITY_REMOVE");
     bytes32 public constant LENDING = keccak256("LENDING");
         bytes32 public constant BORROWING = keccak256("BORROWING");
+    bytes32 public constant STAKING = keccak256("STAKING");
 
 }
