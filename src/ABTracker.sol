b38225b043685cc78e1bbace57c86ed61e0c9292
SynnekOG <ShaneRugget@outlook.com>
2025-12-30 19:14:50 -0800

    feat: Add userActivities Mapping

--- src/ABTracker.sol
+++ src/ABTracker.sol
@@ -29,4 +29,8 @@ contract ABTracker {
         uint256 lastActivityTimestamp;
         mapping(uint256 => bool) activeDays; // Track active days for streak calculation
     }
+
+        // Nested mapping: user => activityType => UserActivity
+    mapping(address => mapping(bytes32 => UserActivity)) private userActivities;
+
 }
