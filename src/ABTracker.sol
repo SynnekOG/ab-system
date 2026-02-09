f862d88507281d3e34ad2e73843d7ace2be6c80a
SynnekOG <ShaneRugget@outlook.com>
2026-02-09 15:03:55 -0800

    feat: Add ActivityConfigUpdated Event

--- src/ABTracker.sol
+++ src/ABTracker.sol
@@ -64,5 +64,6 @@ contract ABTracker {
     event StreakUpdated(address indexed user, bytes32 indexed activityType, uint256 newStreak, uint256 longestStreak);
     event PointsAwarded(address indexed user, bytes32 indexed activityType, uint256 points, uint256 totalPoints);
     event LevelUp(address indexed user, uint256 newLevel, uint256 totalPoints);
+    event ActivityConfigUpdated(bytes32 indexed activityType, bool enabled, uint256 minValue, uint256 pointsMultiplier);
     
 }
